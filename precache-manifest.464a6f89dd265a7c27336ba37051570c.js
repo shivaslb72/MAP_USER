5a75c9cacd68d6b99e5e703e8c1236d68ff6f1e3
Shivakumara Bannimatti <shivaslb72@gmail.com>
2020-04-10 20:19:19 +0530

    Updates

---
+++ precache-manifest.464a6f89dd265a7c27336ba37051570c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbb64d3aceaabb28253e9e24a7077d5d",
+    "url": "/MAP_USER/index.html"
+  },
+  {
+    "revision": "b4a2ed09f4951df82f94",
+    "url": "/MAP_USER/static/css/2.19978504.chunk.css"
+  },
+  {
+    "revision": "1801046e675720a54601",
+    "url": "/MAP_USER/static/css/main.413438a4.chunk.css"
+  },
+  {
+    "revision": "b4a2ed09f4951df82f94",
+    "url": "/MAP_USER/static/js/2.fab6d8d6.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/MAP_USER/static/js/2.fab6d8d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1801046e675720a54601",
+    "url": "/MAP_USER/static/js/main.6514db2b.chunk.js"
+  },
+  {
+    "revision": "2b465f24e2e2429efae5",
+    "url": "/MAP_USER/static/js/runtime-main.53044f56.js"
+  }
+]);
